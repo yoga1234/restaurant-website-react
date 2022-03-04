76b635ed2468496dedc8f1289a86093c1cefc734
Septiana Yoga <septianayoga30@gmail.com>
2022-03-04 19:44:48 +0700

    adding styles for navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantmenu } from "react-icons/md";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import images from "../../constants/images";
 import "./Navbar.css";
@@ -27,6 +27,43 @@ const Navbar = () => (
         <a href="#contact">Contact</a>
       </li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Log In / Register
+      </a>
+      <div></div>
+      <a href="/" className="p__opensans">
+        Book Table
+      </a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={() => {}}
+        />
+        <ul className="app__navbar-smallscreen-links">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
