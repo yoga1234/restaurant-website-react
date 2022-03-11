dcd30b600e2436240295167a4e20233eb8d51957
Septiana Yoga <septianayoga30@gmail.com>
2022-03-11 14:28:29 +0700

    adding subscribe section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -10,6 +10,10 @@ const Newsletter = () => (
       <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
       <p className="p__opensans">And never miss latest Updates!</p>
     </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
