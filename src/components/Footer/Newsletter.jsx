6b83ca35f0a444fe9b35ba1c9995227f4a18bed3
Septiana Yoga <septianayoga30@gmail.com>
2022-03-11 10:31:55 +0700

    initial code for footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
+    </div>
   </div>
 );
 
